8bb50df4331a564ec71b9c182e39ce28724529cf
jkarr_cp <SND\jkarr_cp>
2015-05-21 23:56:39 +0000

    Numbers

---
+++ ExtensionHelpers/Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionHelpers
+{
+    public static partial class ExtensionHelpers
+    {
+        public static bool IsBetween(this long l, long a, long b)
+        {
+            return l >= a && l <= b;
+        }
+
+        public static bool IsBetween(this int i, int a, int b)
+        {
+            return i >= a && i <= b;
+        }
+    }
+}
