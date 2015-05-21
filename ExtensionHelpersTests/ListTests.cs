826d535e90a7d2a115c25f17f3dc4d94ea7e8edd
Johnie Karr <jkarr@users.noreply.github.com>
2015-05-21 22:26:42 +0000

    Lists

---
+++ ExtensionHelpersTests/ListTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ExtensionHelpers;
+
+namespace ExtensionHelpersTests
+{
+    [TestClass]
+    public class ListTests
+    {
+        [TestMethod]
+        public void CopyByValueList()
+        {
+            var newList = testList.CopyByValue();
+
+            Assert.IsFalse(Object.ReferenceEquals(newList, testList));
+            Assert.IsTrue(newList.Count == testList.Count);
+        }
+
+        private List<string> testList = new List<string>
+        {
+            { "Bob" },
+            { "Sally" },
+            { "Joe" }
+        };
+    }
+}
