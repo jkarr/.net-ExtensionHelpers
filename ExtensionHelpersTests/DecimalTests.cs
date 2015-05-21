a7b1b0b74ac957a54216581f57eed3517c38dc24
jkarr_cp <SND\jkarr_cp>
2015-05-21 23:10:51 +0000

    Decimal

---
+++ ExtensionHelpersTests/DecimalTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtensionHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionHelpers.Tests
+{
+    [TestClass()]
+    public class DecimalTests
+    {
+        private decimal testDecimal = 59.74M;
+
+        [TestMethod()]
+        public void ToMoneyDecimalTest()
+        {
+            Assert.IsTrue(testDecimal.ToMoney() == "$59.74");
+        }
+
+        [TestMethod()]
+        public void ToPercentDecimalTest()
+        {
+            Assert.IsTrue(testDecimal.ToPercent() == "5,974.00 %");
+        }
+    }
+}
