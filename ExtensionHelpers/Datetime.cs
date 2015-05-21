85df33b496f09c9a139d1e73aa500116ae165a66
Johnie Karr <jkarr@users.noreply.github.com>
2015-05-21 23:42:34 +0000

    DateTime

---
+++ ExtensionHelpers/Datetime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionHelpers
+{
+    public static partial class ExtensionHelpers
+    {
+        public static bool IsBetween(this DateTime dateTime, DateTime from, DateTime to)
+        {
+            return dateTime >= from && dateTime <= to;
+        }
+
+        public static bool IsBeforeOrEqual(this DateTime dateTime, DateTime compareDate)
+        {
+            return dateTime <= compareDate;
+        }
+
+        public static bool IsAfterOrEqual(this DateTime dateTime, DateTime compareDate)
+        {
+            return dateTime >= compareDate;
+        }
+    }
+}
---
+++ ExtensionHelpersTests/DateTimeTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtensionHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionHelpers.Tests
+{
+    [TestClass()]
+    public class DateTimeTests
+    {
+        private DateTime wayBack = new DateTime(1900, 05, 15, 14, 23, 17);
+        private DateTime someBack = new DateTime(1970, 05, 15, 14, 23, 17);
+        private DateTime future = new DateTime(3000, 05, 15, 14, 23, 17);
+
+        [TestMethod()]
+        public void BetweenDateTimeTest()
+        {
+            Assert.IsTrue(someBack.IsBetween(wayBack, future));
+        }
+
+        [TestMethod()]
+        public void IsBeforeOrEqualeDateTimeTest()
+        {
+            Assert.IsTrue(wayBack.IsBeforeOrEqual(future));
+        }
+
+        [TestMethod()]
+        public void IsAfterOrEqualDateTimeTest()
+        {
+            Assert.IsTrue(future.IsAfterOrEqual(wayBack));
+        }
+    }
+}
