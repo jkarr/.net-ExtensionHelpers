b7a9af14144d795d8b140bc042d4cf9a301ba4c6
Johnie Karr <jkarr@users.noreply.github.com>
2015-05-21 23:10:51 +0000

    Decimal

---
+++ ExtensionHelpers/Decimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionHelpers
+{
+    public static partial class ExtensionHelpers
+    {
+        public static string ToMoney(this decimal value)
+        {
+            return string.Format("{0:C}", value);
+        }
+
+        public static string ToPercent(this decimal value)
+        {
+            return String.Format("{0:P2}", value);
+        }
+    }
+}
