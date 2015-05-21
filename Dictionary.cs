8ac831f9ee89175190106e85390881d19f331f5c
jkarr_cp <SND\jkarr_cp>
2015-05-21 22:13:55 +0000

    Dictionary Extensions

---
+++ Dictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionHelpers
+{
+    public static partial class ExtensionHelpers
+    {
+        /// <summary>
+        /// Serializes a dictionary to a list
+        /// </summary>
+        public static List<KeyValuePair<T1, T2>> Serialize<T1, T2>(this IDictionary<T1, T2> dictionary)
+        {
+            List<KeyValuePair<T1, T2>> list = new List<KeyValuePair<T1, T2>>();
+            list.AddRange(dictionary);
+            return list;
+        }
+
+        /// <summary>
+        /// Add a range of KeyValuePairs to the dictionary
+        /// </summary>
+        /// <param name="dictionary">The dictionary to add to.</param>
+        /// <param name="entries">The range of values to add.</param>
+        /// <param name="overrideEntries">True to override values, whose keys exist in both the entries and the dictionary collections.</param>
+        public static void AddRange<T1, T2>(this IDictionary<T1, T2> dictionary, IDictionary<T1, T2> entries, bool overrideEntries = false)
+        {
+            foreach(var entry in entries)
+            {
+                if(dictionary.ContainsKey(entry.Key))
+                {
+                    if(overrideEntries)
+                    {
+                        dictionary[entry.Key] = entry.Value;
+                    }
+                }
+                else
+                {
+                    dictionary.Add(entry.Key, entry.Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Copies a dictionary by value.
+        /// </summary>
+        /// <param name="dictionary">the dictionary to copy</param>
+        /// <returns>A new dictionary</returns>
+        public static Dictionary<T1, T2> CopyByValue<T1, T2>(this IDictionary<T1, T2> dictionary)
+        {
+            var newDictionary = new Dictionary<T1, T2>();
+            newDictionary.AddRange(dictionary);
+
+            return newDictionary;
+        }
+    }
+}
---
+++ ExtensionHelpersTests/DictionaryTests.cs
@@ -0,0 +1,67 @@
+﻿using ExtensionHelpers;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ExtensionHelpers.Tests
+{
+    [TestClass()]
+    public class DictionaryTests
+    {
+        [TestMethod()]
+        public void SerializeDictionaryTest()
+        {
+            var list = testDictionary.Serialize();
+            Assert.IsTrue(list.Count == testDictionary.Count);
+            Assert.IsTrue(list[0].Key == 1 && list[0].Value == "Bob");
+        }
+
+        [TestMethod]
+        public void AddRangeDictionaryNoOverrideTest()
+        {
+            testDictionary.AddRange(testDictionary2, false);
+            Assert.IsTrue(testDictionary.Count == 5);
+            Assert.IsTrue(testDictionary[1] == "Bob");
+        }
+
+        [TestMethod]
+        public void AddRangeDictionaryOverrideTest()
+        {
+            testDictionary.AddRange(testDictionary2, true);
+            Assert.IsTrue(testDictionary.Count == 5);
+            Assert.IsTrue(testDictionary[1] == "Cain");
+        }
+
+        [TestMethod]
+        public void CopyByValueDictionary()
+        {
+            var newDictionary = testDictionary.CopyByValue();
+            Assert.IsFalse(Object.ReferenceEquals(newDictionary, testDictionary));
+            Assert.IsTrue(newDictionary.Count == testDictionary.Count);
+        }
+
+        private void scrubData()
+        {
+            testDictionary = new Dictionary<int, string>
+            {
+                { 1, "Bob" },
+                { 2, "Alice" },
+                { 3, "John" }
+            };
+        }
+
+        private Dictionary<int, string> testDictionary = new Dictionary<int, string>
+        {
+            { 1, "Bob" },
+            { 2, "Alice" },
+            { 3, "John" }
+        };
+
+        private Dictionary<int, string> testDictionary2 = new Dictionary<int, string>
+        {
+            { 1, "Cain" },
+            { 4, "Sue" },
+            { 5, "Allen" }
+        };
+    }
+}
