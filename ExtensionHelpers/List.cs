b5d1c16d87e198e3b4161999a64a5e35fc725252
jkarr_cp <SND\jkarr_cp>
2015-05-21 22:26:42 +0000

    Lists

---
+++ ExtensionHelpers/List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionHelpers
+{
+    public static partial class ExtensionHelpers
+    {
+        public static IList<T> CopyByValue<T>(this IList<T> list)
+        {
+            var newList = new List<T>();
+            foreach(var item in list)
+            {
+                newList.Add(item);
+            }
+
+            return newList;
+        }
+    }
+}
