f8d8e2ad4e39a8cfa9cbde382eea928a673bc524
Johnie Karr <jkarr@users.noreply.github.com>
2015-05-21 23:31:26 +0000

    Enums

---
+++ ExtensionHelpersTests/EnumTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtensionHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionHelpers.Tests
+{
+    [TestClass()]
+    public class EnumTests
+    {
+        private enum Colors
+        {
+            [System.ComponentModel.Description("Color Blue")]
+            Blue,
+
+            [System.ComponentModel.Description("Color White")]
+            White,
+
+            [Obsolete("Use Purple Instead")]
+            Violet,
+
+            [System.ComponentModel.Description("Color Purple")]
+            Purple
+        }
+        [TestMethod()]
+        public void ToDescriptionEnumTest()
+        {
+            Assert.IsTrue(Colors.Blue.ToDescription() == "Color Blue");
+        }
+
+        [TestMethod()]
+        public void IsObsoleteEnumTest()
+        {
+            Assert.IsTrue(Colors.Violet.IsObsolete());
+            Assert.IsFalse(Colors.White.IsObsolete());
+        }
+
+        [TestMethod()]
+        public void ToEnumFromIntEnumTest()
+        {
+            var i = 1;
+            Assert.IsTrue(i.ToEnum<Colors>() == Colors.White);
+        }
+
+        [TestMethod()]
+        public void ToEnumFromString()
+        {
+            Assert.IsTrue("Purple".ToEnum<Colors>() == Colors.Purple);
+        }
+    }
+}
