fcc1b6479b1a02dceacab2e50c3ccf11b30b8f44
Johnie Karr <jkarr@users.noreply.github.com>
2015-05-21 23:56:39 +0000

    Numbers

---
+++ ExtensionHelpersTests/NumberTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtensionHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionHelpers.Tests
+{
+    [TestClass()]
+    public class NumberTests
+    {
+        private long l = 85743;
+        private int i = 463;
+
+        [TestMethod()]
+        public void IsBetweenNumberLongTest()
+        {
+            Assert.IsTrue(l.IsBetween(1, 999999));
+        }
+
+        [TestMethod()]
+        public void IsBetweenNumberIntTest()
+        {
+            Assert.IsTrue(i.IsBetween(1, 999));
+        }
+    }
+}
